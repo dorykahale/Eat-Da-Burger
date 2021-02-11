fdd07a399e439a53826810f275f9032cf6a33d47
DMC <dorykahale@gmail.com>
2021-02-11 09:46:26 -0600

    testing

--- config/connection.js
+++ config/connection.js
@@ -1,38 +1,6 @@
-// require('dotenv').config();
-// // Set up MySQL connection
-// var mysql = require("mysql");
-// var connection;
-
-// if (process.env.JAWSDB_URL) {
-//   // connect to JawsDB (heroku)
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   // else connect to local database
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "rootroot",
-//     database: "burgers_db"
-//   });
-// }
-
-// // Make connection
-// connection.connect(function (err) {
-//   if (err) {
-//     console.error("Error connecting: " + err.stack + "\n");
-//     return;
-//   }
-//   console.log("Connected as id " + connection.threadId + "\n");
-// });
-
-// // Export connection for our ORM to use.
-// module.exports = connection;
-
-
-// Set up MySQL connection.
 var mysql = require("mysql");
-
 var connection;
+
 if(process.env.JAWSDB_URL) {
     conncetion =mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -54,3 +22,4 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
+//testing
