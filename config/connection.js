d9bbd13fecbd6cc0c2bd2252cf0e891a5e8bc070
DMC <dorykahale@gmail.com>
2021-02-11 01:09:36 -0600

    another try at connection

--- config/connection.js
+++ config/connection.js
@@ -1,53 +1,56 @@
-require('dotenv').config();
-// Set up MySQL connection
-var mysql = require("mysql");
-var connection;
-
-if (process.env.JAWSDB_URL) {
-  // connect to JawsDB (heroku)
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  // else connect to local database
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "rootroot",
-    database: "burgers_db"
-  });
-}
-
-// Make connection
-connection.connect(function (err) {
-  if (err) {
-    console.error("Error connecting: " + err.stack + "\n");
-    return;
-  }
-  console.log("Connected as id " + connection.threadId + "\n");
-});
-
-// Export connection for our ORM to use.
-module.exports = connection;
-
-
-// Set up MySQL connection.
+// require('dotenv').config();
+// // Set up MySQL connection
 // var mysql = require("mysql");
+// var connection;
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "rootroot",
-//   database: "burgers_db"
-// });
+// if (process.env.JAWSDB_URL) {
+//   // connect to JawsDB (heroku)
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   // else connect to local database
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "rootroot",
+//     database: "burgers_db"
+//   });
+// }
 
-// // Make connection.
-// connection.connect(function(err) {
+// // Make connection
+// connection.connect(function (err) {
 //   if (err) {
-//     console.error("error connecting: " + err.stack);
+//     console.error("Error connecting: " + err.stack + "\n");
 //     return;
 //   }
-//   console.log("connected as id " + connection.threadId);
+//   console.log("Connected as id " + connection.threadId + "\n");
 // });
 
 // // Export connection for our ORM to use.
 // module.exports = connection;
+
+
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection;
+if(process.env.JAWSDB_URL) {
+    conncetion =mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "rootroot",
+        database: "burgers_db"
+    });
+}
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
